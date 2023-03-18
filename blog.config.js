18df1e43a57e1381f0524f5669d0db1e710c6a2b
aimeedikkersixg42 <128145122+aimeedikkersixg42@users.noreply.github.com>
2023-03-18 22:42:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Kaedeleo', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'ActiveDirectory | Powershell', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xifanliang.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://kaedeleo.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Powershell', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
